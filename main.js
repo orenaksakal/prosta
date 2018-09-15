9252bf9157ec65d6f9fe06d440184ad9c342ddcc
Oren Aksakal <orenaksakal@gmail.com>
2018-09-15 17:15:32 +0200

    development done

--- main.js
+++ main.js
@@ -2,77 +2,40 @@
  * Author: Oren Aksakal <oren.aksakal@oracle.com>
  * Date: 7/1/18
  */
-//
-// chrome.storage.onChanged.addListener(function (changes) {
-//     if (changes.editableText) {
-//         document.body.contentEditable = changes.editableText.newValue;
-//     }
-// });
-//
-// chrome.storage.local.get(['editableText'], function (result) {
-//     if (result.editableText) {
-//         document.body.contentEditable = result.editableText;
-//     } else {
-//         document.body.contentEditable = false;
-//     }
-// });
 
-//TODO: donot reacreate stype, replace it
-function applyChangesToSelection(messageObj) {
-    var range = window.getSelection().getRangeAt(0);
-    var span = document.createElement('span');
-    var css = document.createElement('style');
-    var spanClass = 'prosta-highlight-' + Date.now();
-    
-    function createStyle() {
+const applyChangesToSelection = (messageObj) => {
+    const range = window.getSelection().getRangeAt(0);
+    const span = document.createElement('span');
+    const css = document.createElement('style');
+    const spanClass = 'prosta-highlight-' + Date.now();
+
+    const createStyle = () => {
         css.type = 'text/css';
         css.id = 'prosta';
         css.innerHTML = '.' + spanClass + ' { font-size:' + messageObj.fontsize + '!important; color:' + messageObj.fontcolor + '!important; font-weight:' + messageObj.fontweight + '!important;}';
         document.body.appendChild(css);
-    }
-    
-    span.className = spanClass;
+    };
+
+    span.classList.add(spanClass);
     span.appendChild(range.extractContents());
     range.insertNode(span);
     createStyle();
-}
+};
 
 chrome.runtime.onMessage.addListener(function (messageObj, sender, sendResponse) {
     if (messageObj.messageName === 'initiate') {
         sendResponse({
             selection: window.getSelection().toString(),
+            draggable: document.body.draggable,
+            editable: document.designMode !== 'off'
         });
     } else if (messageObj.messageName === 'applyChanges') {
         applyChangesToSelection(messageObj);
     }
+    else if (messageObj.messageName === 'editable') {
+        let state = messageObj.state ? 'on' : 'off';
+        document.designMode = state;
+    } else if (messageObj.messageName === 'draggable') {
+        document.body.draggable = messageObj.state;
+    }
 });
-
-//
-// // TODO: create element and set class for it and wrap selection by it so you can style
-// // var selectedRange = window.getSelection().getRangeAt(0);
-// // var newNode = document.createElement('div');
-// // newNode.classList.add('selection');
-// // selectedRange.surroundContents(newNode);
-
-// function applyChangesToSelection() {
-//     var range = window.getSelection().getRangeAt(0);
-//     var span = document.createElement('span');
-//     var css = document.createElement("style");
-//
-//     css.type = "text/css";
-//     css.innerHTML = ".hightlight { font-size:55px; color: red; }";
-//     document.body.appendChild(css);
-//
-//     span.className = 'highlight';
-//     span.appendChild(range.extractContents());
-//     range.insertNode(span);
-//
-//
-// }
-
-// var range = window.getSelection().getRangeAt(0);
-// var span = document.createElement('span');
-//
-// span.className = 'highlight';
-// span.appendChild(range.extractContents());
-// range.insertNode(span);
--- popup.js
+++ popup.js
@@ -1,86 +1,81 @@
-// /**
-//  * Author: Oren Aksakal <oren.aksakal@oracle.com>
-//  * Date: 7/1/18
-//  */
-//
-// const elementIDs = {
-//     checkBoxes: ['editableText', 'draggableElements'],
-//     inputBoxes: ['fontFamily', 'fontSize', 'fontColor'],
-// };
-//
-// const handleStateChange = (box) => {
-//     chrome.storage.local.set({ box: this.checked });
-// };
-//
-// elementIDs.checkBoxes.forEach(checkBox => {
-//     document.getElementById(checkBox).addEventListener('change', handleStateChange.bind(checkBox));
-// });
-//
-// elementIDs.inputBoxes.forEach(inputBox => {
-//     document.getElementById(inputBox).addEventListener('keyup', handleStateChange.bind(inputBox));
-// });
-//
-// /**
-//  * Sets box states on popup open to the latest state
-//  */
-// (() => {
-//     elementIDs.checkBoxes.forEach(elementIdentifier => {
-//         chrome.storage.local.get([elementIdentifier], function (result) {
-//             let element = document.getElementById(elementIdentifier);
-//             element.checked = result.elementIdentifier;
-//         });
-//     });
-//
-//     elementIDs.inputBoxes.forEach(elementIdentifier => {
-//         chrome.storage.local.get([elementIdentifier], function (result) {
-//             let element = document.getElementById(elementIdentifier);
-//             element.value = result.elementIdentifier;
-//         });
-//     });
-// })();
-//
+/**
+ * Author: Oren Aksakal <oren.aksakal@oracle.com>
+ * Date: 7/1/18
+ */
 
-function sendMessage(messageObject, callback) {
+const elementIDs = ['editable', 'draggable'];
+
+const sendMessage = (messageObject, callback) => {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         chrome.tabs.sendMessage(tabs[0].id, messageObject, function (response) {
             callback(response);
         });
     });
-}
+};
 
-function handleInitializationResponse(response) {
+elementIDs.forEach((selector) => {
+    let elementNode = document.getElementById(selector);
+
+    elementNode.addEventListener('change', () => {
+        sendMessage({
+            messageName: selector,
+            state: elementNode.checked
+        }, function () {
+            // do nothing
+        });
+    });
+});
+
+const handleInitializationResponse = (response) => {
     if (response.selection !== '') {
         document.getElementById('output').innerHTML = response.selection;
     } else {
         document.getElementById('output').innerHTML = 'No text was selected, you are given this one to play with.';
     }
-}
+
+    if (response.editable) {
+        document.getElementById('editable').checked = response.editable;
+    }
+
+    if (response.draggable) {
+        document.getElementById('draggable').checked = response.draggable;
+    }
+};
 
 
-fontfamily.addEventListener('input', function () {
+fontfamily.addEventListener('input', () => {
     document.getElementById('output').style.fontFamily = fontfamily.value;
 });
 
-fontsize.addEventListener('input', function () {
+fontsize.addEventListener('input', () => {
     document.getElementById('output').style.fontSize = fontsize.value;
 });
 
-fontcolor.addEventListener('input', function () {
+fontcolor.addEventListener('input', () => {
     document.getElementById('output').style.color = fontcolor.value;
 });
 
-fontweight.addEventListener('input', function () {
+fontweight.addEventListener('input', () => {
     document.getElementById('output').style.fontWeight = fontweight.value;
 });
 
-save.addEventListener('click', function () {
-    sendMessage({
-        messageName: 'applyChanges',
-        fontfamily: fontfamily.value,
-        fontsize: fontsize.value,
-        fontcolor: fontcolor.value,
-        fontweight: fontweight.value,
-    });
+save.addEventListener('click', () => {
+    if (fontfamily.value && fontfamily.value !== ' ' ||
+        fontsize.value && fontsize.value !== ' ' ||
+        fontcolor.value && fontcolor.value !== ' ' ||
+        fontweight.value && fontweight.value !== ' ') {
+        sendMessage({
+            messageName: 'applyChanges',
+            fontfamily: fontfamily.value,
+            fontsize: fontsize.value,
+            fontcolor: fontcolor.value,
+            fontweight: fontweight.value
+        }, function () {
+            // do nothing
+        });
+    }
 });
 
-sendMessage({ messageName: 'initiate' }, handleInitializationResponse);
+sendMessage({ messageName: 'initiate' }, (response) => {
+    handleInitializationResponse(response);
+});
